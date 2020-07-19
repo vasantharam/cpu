a8f8258b951945dd72470306d0b5a4f5e7db4121
vasantharam <vasantharam@gmail.com>
2020-07-19 15:11:50 +0530

    compiling version of cdcfifo

--- rtl/cdc_fifo/tb.v
+++ rtl/cdc_fifo/tb.v
@@ -1,40 +1,38 @@
 module top;
     parameter FIFO_WIDTH=8;
-    parameter FIFO_DEPTH=256;
-    reg readValid, writeValid, clk, rst;
+    parameter FIFO_DEPTH=255;
+    reg readValid, writeValid, wrclk, rdclk, rst;
     reg  [FIFO_WIDTH-1:0] writeData;
     reg  [FIFO_WIDTH-1:0] readData;
     reg readReady, writeReady;
     reg readPending, writePending;
     integer readFrequency, writeFrequency;
-    fifo # (.FIFO_DEPTH(256), .FIFO_WIDTH(8)) my_fifo (readReady, writeReady, readValid, writeValid, writeData, readData, clk, rst);
+    cdcfifo # (.FIFO_DEPTH(255), .FIFO_WIDTH(8)) my_fifo (readReady, writeReady, readValid, writeValid, writeData, readData,rdclk, wrclk, rst);
     initial 
     begin
-        clk <= 0;
+        wrclk <= 0;
+        rdclk <= 0;
         rst <= 1;
         readPending <= 0;
         writePending <= 1;
-        readValid <= 0;
-        writeValid <= 0;
         if (!$value$plusargs("readFrequency=%d", readFrequency)) readFrequency <= 3;
         if (!$value$plusargs("writeFrequency=%d", writeFrequency)) writeFrequency <= 5;
         $display("readFrequency = %d", readFrequency);
         $display("writeFrequency = %d", writeFrequency);
     #30 rst <= 0;
-    #1000000 $finish();
     end
     always 
     begin
-        #10 clk <= ~clk;
+        #10 rdclk <= ~rdclk;
+        #10 wrclk <= ~wrclk;
     end
 
-    always @(posedge clk)
+    always @(posedge wrclk)
     begin
         reg writing_this_cycle;
         
-
         writing_this_cycle <= (($urandom % writeFrequency) ==0)?1:0;
-        
+        $display("%x: writing_this_cycle = %d", $time(), writing_this_cycle);
         if (!rst)
         begin
         if (!writePending) begin
@@ -47,8 +45,14 @@ module top;
         if (writeReady) writePending <=0;
         else if (!writePending && writing_this_cycle) writePending <= 1;
 
-//        if (readReady == 1 && readValid == 1) $display ("%x: Read Data:  %x ", $time(), readData);
- //       if (writeReady == 1 && writeValid == 1) $display ("%x: Write Data:  %x ", $time(), writeData);
+        if (writeReady == 1 && writeValid == 1) $display ("%x: Write Data:  %x ", $time(), writeData);
+        end
+    end
+    always @(posedge rdclk)
+    begin
+        if (!rst)
+        begin
+        if (readReady == 1 && readValid == 1) $display ("%x: Read Data:  %x ", $time(), readData);
         if (!readPending) begin
             if (($urandom%readFrequency) == 0) begin
                 readValid <= 1;
@@ -56,8 +60,7 @@ module top;
                 else readPending <= 1;
             end
             else readValid <= 0;
-        end
-        end
+        end //!readPending
+        end //!rst
     end 
 endmodule
-

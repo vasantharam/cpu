def46cb2752dec5b6d92611be68f1d304cc26653
Ramasubramanian Balasubramanian <vasantharam@gmail.com>
2020-10-24 18:01:33 +0530

    Adding more pseudo-code. RAM -> multiple read/write ports

--- rtl/eth/eth_reader.v
+++ rtl/eth/eth_reader.v
@@ -1,12 +1,9 @@
 /* In this limited implementation `ETH_MAX_FRAME_SIZE is restricted to 256
 * bits */
-`define RD_STATE_IDLE 0
-`define RD_STATE_WAIT_FOR_RX_DATA 1
-`define RD_STATE_WRITE_FRAME_TO_RAM 2
-`define RD_STATE_WAIT_SEND_DONE 3
 
+`include "eth_defs.vh" 
 module  eth_reader 
-    #(parameter DATA_WIDTH_MSB=15, parameter ETH_MAX_FRAME_SIZE=256) 
+    #(parameter DATA_WIDTH_MSB=15, parameter ETH_MAX_FRAME_SIZE=256, parameter ADDR_WIDTH_MSB=15) 
       (reg_ether_READ_FRAME_BASE, 
        reg_ether_READ_FRAME_RD_PTR, 
        reg_ether_READ_FRAME_WR_PTR, 
@@ -15,7 +12,7 @@ module  eth_reader
 
        ram_wr_data,  ram_wr_valid,  ram_wr_addr,  ram_wr_ready,  
        
-       rx_drv_rd_data,  rx_drv_rd_valid,  rx_drv_rd_ready, clk, rst )
+       rx_drv_rd_data,  rx_drv_rd_valid,  rx_drv_rd_ready, clk, rst );
 /* reg slave internal interface */
 input [DATA_WIDTH_MSB:0] reg_ether_READ_FRAME_BASE; 
 inout [DATA_WIDTH_MSB:0]  reg_ether_READ_FRAME_RD_PTR;
@@ -25,7 +22,7 @@ output reg [3:0] read_fsm_state;
 /* Ram interface */
 output reg [DATA_WIDTH_MSB:0] ram_wr_data;
 output reg ram_wr_valid; 
-output reg ram_wr_addr; 
+output reg [ADDR_WIDTH_MSB:0] ram_wr_addr; 
 input ram_wr_ready;
 
 /* rx_drv internal interface */
@@ -43,7 +40,7 @@ always @(posedge clk)
 begin
     if (rst)
     begin
-        read_fsm_state <= `RD_ST_IDLE;
+        read_fsm_state <= `RD_STATE_IDLE;
         ram_wr_valid <= 0;
         rx_drv_rd_valid <= 0;
     end
@@ -51,11 +48,13 @@ begin
     begin
         case (read_fsm_state)
         `RD_STATE_IDLE:
-            rx_drv_rd_valid <= 1;
-            read_fsm_state <= WAIT_FOR_RX_FRAME;
+		      begin
+                rx_drv_rd_valid <= 1;
+                read_fsm_state <= `RD_STATE_WAIT_FOR_RXDATA;
+				end
         `RD_STATE_WAIT_FOR_RXDATA:
             begin
-            assert (rx_drv_rd_valid) else $error ("Unexpected FSM state WAIT_FOR_RXDATA");
+//            assert (rx_drv_rd_valid) else $error ("Unexpected FSM state WAIT_FOR_RXDATA");
             if (rx_drv_rd_ready && rx_drv_rd_valid)
             begin
                 read_frame <= rx_drv_rd_data;
@@ -67,11 +66,12 @@ begin
                 read_fsm_state <= `RD_STATE_WRITE_FRAME_TO_RAM;
                 frame_size_counter <= 0;
             end
+				end
         `RD_STATE_WRITE_FRAME_TO_RAM:
             begin
                 if (ram_wr_ready == 1 && ram_wr_valid == 1)
                 begin
-                    wr_ptr <= wr_ptr + (DATA_WIDTH_MSB+1)/8
+                    wr_ptr <= wr_ptr + (DATA_WIDTH_MSB+1)/8;
                     ram_wr_data <= rx_drv_rd_data[(ETH_MAX_FRAME_SIZE - frame_size_counter):(ETH_MAX_FRAME_SIZE - DATA_WIDTH_MSB - frame_size_counter)];
                 
                     frame_size_counter <= frame_size_counter + (DATA_WIDTH_MSB+1)/8; 
@@ -82,6 +82,7 @@ begin
                         read_fsm_state <= `RD_STATE_WAIT_LAST_WRITE;
                     end
                     ram_wr_addr <= reg_ether_READ_FRAME_BASE + wr_ptr;
+				    end
             end
         `RD_STATE_WAIT_LAST_WRITE:
             begin
@@ -91,7 +92,8 @@ begin
                     read_fsm_state <= `RD_STATE_IDLE;
                 end
             end
-    end
+        endcase
+	 end
 end
 
 endmodule
--- rtl/eth/rx_driver.v
+++ rtl/eth/rx_driver.v
@@ -1,3 +1,4 @@
+`include "eth_defs.vh"
 module eth_rx_driver(rx_drv_rd_data, rx_drv_rd_valid, rx_drv_rd_ready, clk, rst);
 
 endmodule
